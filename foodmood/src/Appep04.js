4eb13a5f39c82083af0d306e8778e59d2d31671b
ansh-mehta7 <anshmehta630@gmail.com>
2024-05-18 00:49:27 +0530

    modified the app

--- foodmood/src/Appep04.js
+++ foodmood/src/Appep04.js
@@ -2,19 +2,47 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
-
-
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import About from "./components/About";
+import Error from "./components/Error";
+import Contact from "./components/Contact";
+import RestrauntMenu from "./components/RestrauntMenu";
 
 // swiggy api
 
 const AppLayout = () => {
-  
   return (
     <div className="app">
       <Header />
-      <Body />
+      <Outlet />
     </div>
   );
 };
+// what should happen on a a specific path
+const aprouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Body />,
+      },
+      {
+        path: "/about", // if path is /abot then load my about component
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/restaurant/:resid",
+        element: <RestrauntMenu />,
+      },
+    ],
+    errorElement: <Error />,
+  },
+]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayout />);
+root.render(<RouterProvider router={aprouter} />);
